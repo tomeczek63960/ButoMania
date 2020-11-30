f0ba1fb7128767518abe4b73bbd78eec56226627
Tomek <tomek63960@o2.pl>
2020-11-30 19:28:45 +0100

    change hero slider img on mobile size

--- client/src/jsx/02-molecules/PageHero.js
+++ client/src/jsx/02-molecules/PageHero.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import heroBg3 from 'assets/hero.webp';
 import heroBg1 from 'assets/hero2.webp';
 import heroBg2 from 'assets/hero3.webp';
+import heroBg3 from 'assets/hero.webp';
+
+import heroBg1Sm from 'assets/hero-sm.webp';
+import heroBg2Sm from 'assets/hero-2-sm.webp';
+import heroBg3Sm from 'assets/hero-3-sm.webp';
 
 const PageHero = () => {
     let [ activeSlide, setActiveSlide ] = useState(); 
@@ -29,28 +33,48 @@ const PageHero = () => {
             <header className="page-hero">
                 <div className="page-hero__bg" >
                     <div className="page-hero__bg__slid" >
-                        <img  src={heroBg3} alt="Nike shoes"/>
+                        {
+                            window.innerWidth <= 600 ? 
+                            <img  src={heroBg3Sm} alt="Nike shoes"/>
+                            :
+                            <img  src={heroBg3} alt="Nike shoes"/>
+                        }
                     </div>
                     <div className={`page-hero__bg__slid ${activeSlide === 0 && "page-hero__bg__slid--active" }`}>
                         <div className="page-hero__bg__content">
                             <h3 className='page-hero__bg__heading'>Bogata oferta <br/> butów Nike</h3>
                             <Link to={{pathname:'/filter',state: { filter: { brand: "nike" } } }} className='page-hero__bg__btn' >Zobacz</Link>
                         </div>
-                        <img  src={heroBg3} alt="Nike shoes"/>
+                        {
+                            window.innerWidth <= 600 ? 
+                            <img  src={heroBg3Sm} alt="Nike shoes"/>
+                            :
+                            <img  src={heroBg3} alt="Nike shoes"/>
+                        }
                     </div>
                     <div className={`page-hero__bg__slid ${activeSlide === 1 && "page-hero__bg__slid--active" }`}>
                         <div className="page-hero__bg__content">
                             <h3 className='page-hero__bg__heading'>Powtrót do szkoły <br/> ze stylem</h3>
                             <Link to={{pathname:'/filter',state: { filter: { shoesType: "tenisówki" } } }} className='page-hero__bg__btn' >Zobacz</Link>
                         </div>
-                        <img  src={heroBg1} alt="Nike shoes"/>
+                        {
+                              window.innerWidth <= 600 ? 
+                              <img  src={heroBg1Sm} alt="Nike shoes"/>
+                              :
+                              <img  src={heroBg1} alt="Nike shoes"/>
+                        }
                     </div>
                     <div className={`page-hero__bg__slid ${activeSlide === 2 && "page-hero__bg__slid--active" }`}>
                         <div className="page-hero__bg__content">
                             <h3 className='page-hero__bg__heading'>Najlepsze buty <br/> do biegania</h3>
                             <Link to={{pathname:'/filter',state: { filter: { shoesType: "sneakersy" } } }} className='page-hero__bg__btn' >Zobacz</Link>
                         </div>
-                        <img  src={heroBg2} alt="Nike shoes"/>
+                        {
+                            window.innerWidth <= 600 ? 
+                            <img  src={heroBg2Sm} alt="Nike shoes"/>
+                            :
+                            <img  src={heroBg2} alt="Nike shoes"/>
+                        }
                     </div>
                 </div>
 
